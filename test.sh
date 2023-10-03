1ef20f05a0d20a6d565b596f96a80b3ddfed46b5
Milos Vasic <i@mvasic.ru>
2023-10-03 16:52:13 +0300

    Run sonar scan.

--- test.sh
+++ test.sh
@@ -15,90 +15,110 @@ if [ -n "$1" ]; then
 else
 
   RECIPES="Recipes"
-
 fi
-
-SCRIPT_GET_SONARQUBE="get_sonar_qube.sh"
-RECIPE_SONAR_CUBE="$RECIPES/sonar_qube_parameters.sh"
-
-SCRIPT_GET_SONARQUBE_FULL_PATH="$SUBMODULES_HOME/Software-Toolkit/Utils/SonarQube/$SCRIPT_GET_SONARQUBE"
 
 if test -e "$RECIPES"; then
 
   echo "Using recipes from: $RECIPES"
 
-  if test -e "$RECIPE_SONAR_CUBE"; then
+  RUN_TESTS() {
 
-    # shellcheck disable=SC1090
-    . "$RECIPE_SONAR_CUBE"
+    if [ -n "$SONARQUBE_SERVER" ]; then
 
-    if [ -n "$SONARQUBE_NAME" ]; then
-      
-      echo "$SONARQUBE_NAME test starting"
+      echo "Using external SonarQube instance: $SONARQUBE_SERVER"
 
-    else
-      
-      echo "ERROR: SONARQUBE_NAME is not provided"
-      exit 1
+      # TODO
+
     fi
+  }
 
-    if [ -z "$SONARQUBE_PORT" ]; then
-      
-      SONARQUBE_PORT="9000"
-    fi
+  BRING_SONARQUBE_UP() {
 
-    echo "To bind the port: $SONARQUBE_PORT"
+    if test -e "$RECIPE_SONAR_CUBE"; then
 
-    if test -e "$SCRIPT_GET_SONARQUBE_FULL_PATH"; then
+      # shellcheck disable=SC1090
+      . "$RECIPE_SONAR_CUBE"
 
-      if [ -n "$DB_USER" ]; then
-
-        if [ -z "$DB_PASSWORD" ]; then
-      
-          echo "ERROR: Password parameter is mandatory when DB user is provided for the test"
-          exit 1
-        fi
-
-        if [ -n "$ADMIN_PASSWORD" ]; then
-      
-          if sh "$SCRIPT_GET_SONARQUBE_FULL_PATH" "$SONARQUBE_NAME" "$SONARQUBE_PORT" "$DB_USER" "$DB_PASSWORD" "$ADMIN_PASSWORD"; then
-
-            echo "SonarQube is ready"
-
-          else
-
-            echo "ERROR: SonarQube is not ready (2)"
-            exit 1
-          fi
-
-        else
-
-          if sh "$SCRIPT_GET_SONARQUBE_FULL_PATH" "$SONARQUBE_NAME" "$SONARQUBE_PORT" "$DB_USER" "$DB_PASSWORD"; then
-
-            echo "SonarQube is ready"
-
-          else
-
-            echo "ERROR: SonarQube is not ready (1)"
-            exit 1
-          fi
-        fi
+      if [ -n "$SONARQUBE_NAME" ]; then
         
-      else
+        echo "$SONARQUBE_NAME test starting"
 
-        echo "ERROR: DB user parameter is mandatory"
+      else
+        
+        echo "ERROR: SONARQUBE_NAME is not provided"
         exit 1
       fi
 
-    else
+      if [ -z "$SONARQUBE_PORT" ]; then
+        
+        SONARQUBE_PORT="9000"
+      fi
 
-      echo "ERROR: Not found '$SCRIPT_GET_SONARQUBE_FULL_PATH'"
-      exit 1
+      echo "To bind the port: $SONARQUBE_PORT"
+
+      if test -e "$SCRIPT_GET_SONARQUBE_FULL_PATH"; then
+
+        if [ -n "$DB_USER" ]; then
+
+          if [ -z "$DB_PASSWORD" ]; then
+        
+            echo "ERROR: Password parameter is mandatory when DB user is provided for the test"
+            exit 1
+          fi
+
+          if [ -n "$ADMIN_PASSWORD" ]; then
+        
+            if sh "$SCRIPT_GET_SONARQUBE_FULL_PATH" "$SONARQUBE_NAME" "$SONARQUBE_PORT" "$DB_USER" "$DB_PASSWORD" "$ADMIN_PASSWORD"; then
+
+              echo "SonarQube is ready"
+
+            else
+
+              echo "ERROR: SonarQube is not ready (2)"
+              exit 1
+            fi
+
+          else
+
+            if sh "$SCRIPT_GET_SONARQUBE_FULL_PATH" "$SONARQUBE_NAME" "$SONARQUBE_PORT" "$DB_USER" "$DB_PASSWORD"; then
+
+              echo "SonarQube is ready"
+
+            else
+
+              echo "ERROR: SonarQube is not ready (1)"
+              exit 1
+            fi
+          fi
+          
+        else
+
+          echo "ERROR: DB user parameter is mandatory"
+          exit 1
+        fi
+
+      else
+
+        echo "ERROR: Not found '$SCRIPT_GET_SONARQUBE_FULL_PATH'"
+        exit 1
+      fi
     fi
+  }
 
-    echo "ERROR: Sonar Cube support to be implemented"
-    exit 1
+  SCRIPT_GET_SONARQUBE="get_sonar_qube.sh"
+  RECIPE_SONAR_CUBE="$RECIPES/sonar_qube_parameters.sh"
+  SCRIPT_GET_SONARQUBE_FULL_PATH="$SUBMODULES_HOME/Software-Toolkit/Utils/SonarQube/$SCRIPT_GET_SONARQUBE"
+
+  if [ -n "$SONARQUBE_SERVER" ]; then
+
+    echo "Using external SonarQube instance: $SONARQUBE_SERVER"
+
+  else
+
+    BRING_SONARQUBE_UP
   fi
+
+  RUN_TESTS
 
 else
 
